91fae4878553335858a5d94faaee2ffcaf0b223a
Francisco Daniel Souza Fernandes <f.daniel.souza@gmail.com>
2021-12-17 11:50:47 -0300

    Adicionado método de matriz inversa e adjunta

--- algebra_linear.py
+++ algebra_linear.py
@@ -14,6 +14,10 @@ def multiplicar_matrizes(matriz1, matriz2):
     return res
 
 
+def determinante_2x2(matriz):
+    return matriz[0][0] * matriz[1][1] - matriz[0][1] * matriz[1][0]
+
+
 def expansao_laplace(matriz):
     coeficientes = list()
     determinantes = list()
@@ -31,14 +35,13 @@ def expansao_laplace(matriz):
                 mt = transpor_matriz(mt)
 
                 if len(mt) == 2:
-                    det = mt[0][0] * mt[1][1] - mt[0][1] * mt[1][0]
+                    det = determinante_2x2(mt)
                     determinantes.append(det)
 
                 coeficientes_e_submatrizes(mt, linhas_removidas + 1)
 
     coeficientes_e_submatrizes(matriz)
-    tamanhos_coeficientes = [len(c) for c in coeficientes]
-    tamanho_maior_coef = max(tamanhos_coeficientes)
+    tamanho_maior_coef = max([len(c) for c in coeficientes])
 
     for t in range(3, tamanho_maior_coef + 1):
         coefs_superiores = [c for c in coeficientes if len(c) == t]
@@ -48,3 +51,46 @@ def expansao_laplace(matriz):
         ))
 
     return sum(sum(determinantes, []))
+
+
+def matriz_adjunta(matriz):
+    adjunta = [[j for j in i] for i in matriz]
+
+    for i in range(len(matriz[0])):
+        for j in range(len(matriz)):
+            mt = transpor_matriz(matriz)
+            del mt[j]
+            mt = transpor_matriz(mt)
+            del mt[i]
+            det_mt = expansao_laplace(mt) if len(mt) > 2 else determinante_2x2(mt) if len(mt) == 2 else sum(sum(mt, []))
+            cofator = (-1) ** (i + j) * det_mt
+            adjunta[i][j] = cofator
+
+    return adjunta
+
+
+def matriz_inversa(matriz):
+    t = transpor_matriz(matriz)
+    det = expansao_laplace(t) if len(t) > 2 else determinante_2x2(t)
+    adj = matriz_adjunta(transpor_matriz(matriz))
+    return [[j / det for j in i] for i in adj]
+
+
+
+
+m = [
+    [4, 5, -3, 0, 3, 3],
+    [2, -1, 3, 1, 2, -2],
+    [1, -3, 2, 1, 4, 5],
+    [1, 2, -2, 5, 3, -5],
+    [2, 4, -1, 1, -2, -4],
+    [-2, -1, 1, 1, 7, 1]
+]
+
+#m = [[4, 3, 2], [4, 5, 6], [7, 9, 8]]
+#m = [[1, 2, 3], [0, 1, 4], [5, 6, 0]]
+
+
+#m = [[-2, 1], [0, 0]]
+print(matriz_inversa(m))
+
